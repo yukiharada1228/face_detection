4f63c094120f7b619615ec75aa43addfd356da55
yukiharada1228 <yukiharada1228@gmail.com>
2022-11-25 11:57:11 +0900

    Update config.py
    重みファイルの自動ダウンロード機能を追加

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 import sys
+import subprocess
 
 import cv2 as cv
 
@@ -11,8 +12,13 @@ logging.basicConfig(level=logging.DEBUG,
                         stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
+CMD_OMZ = 'omz_downloader --name face-detection-retail-0005'
+
 PROJECT_ROOT = Path(__file__).resolve().parent
-MODEL_PATH = PROJECT_ROOT / 'intel/face-detection-retail-0005/FP16/face-detection-retail-0005'
+MODEL_DIR = PROJECT_ROOT / 'intel'
+if not MODEL_DIR.exists():
+    subprocess.call(CMD_OMZ.split(' '))
+MODEL_PATH = MODEL_DIR / 'face-detection-retail-0005/FP16/face-detection-retail-0005'
 CAMERA_PORT = 0
 logger.debug({'PROJECT_ROOT': PROJECT_ROOT,
               'MODEL_PATH': MODEL_PATH,
