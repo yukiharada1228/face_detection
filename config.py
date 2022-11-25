e52b4da5a9e380f2d5302d779ffa57b197fce4e0
yukiharada1228 <117978472+yukiharada1228@users.noreply.github.com>
2022-11-26 01:41:59 +0900

    Update config.py

--- config.py
+++ config.py
@@ -17,12 +17,11 @@ CMD_OMZ = 'omz_downloader --name face-detection-retail-0005'
 PROJECT_ROOT = Path(__file__).resolve().parent
 MODEL_DIR = PROJECT_ROOT / 'intel'
 if not MODEL_DIR.exists():
-    subprocess.call(CMD_OMZ.split(' '))
+    subprocess.call(CMD_OMZ.split(' '), cwd=str(PROJECT_ROOT))
 MODEL_PATH = MODEL_DIR / 'face-detection-retail-0005/FP16/face-detection-retail-0005'
 CAMERA_PORT = 0
 logger.debug({'PROJECT_ROOT': PROJECT_ROOT,
               'MODEL_PATH': MODEL_PATH,
               'CAMERA_PORT': CAMERA_PORT})
 
-capture = cv.VideoCapture(CAMERA_PORT)
 face_detect = FaceDetect(model_path=str(MODEL_PATH))
